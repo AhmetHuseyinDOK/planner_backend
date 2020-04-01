b82c091086c5dc47450699bb70831b3290f0c56e
Ahmet Huseyin Dok <ahmet.dok@intern.generalmobile.com>
2020-04-01 15:34:24 +0300

    dotenv

--- routes/plan.js
+++ routes/plan.js
@@ -4,6 +4,7 @@ const router  = new Router();
 const PlanController  = require('../controllers/plan');
 
 router.post('/plan',PlanController.create);
+router.get('/plan/:id/attendee/:attendeeId',PlanController.getAttendee);
 router.put('/plan/:id/attendee',PlanController.putAttendee);
 router.get('/plan/:id',PlanController.get);
 router.get('/plan/:id/detailed',PlanController.getDetailed);
